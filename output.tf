1b63d904b41c63b5b19a59e33c68687a9ae26caf
priteshghaywat <87220640+priteshghaywat@users.noreply.github.com>
2024-11-16 22:57:59 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,3 @@
 output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = azurerm_resource_group.example.name
-}
-
-output "resource_group_location" {
-  description = "The location of the resource group"
-  value       = azurerm_resource_group.example.location
+  value = azurerm_resource_group.rg.name
 }
