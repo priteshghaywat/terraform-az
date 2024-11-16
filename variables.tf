1ca1fca42055158eba66ecb94c097ed63eb3d647
priteshghaywat <87220640+priteshghaywat@users.noreply.github.com>
2024-11-16 22:58:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,4 @@
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-  default     = "my-terraform-rg"
-}
-
-variable "location" {
-  description = "The location where the resource group will be created"
-  type        = string
-  default     = "East US"
-}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
