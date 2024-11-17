3ac3970613e1951424994ebeb1dce0262ab342e3
priteshghaywat <87220640+priteshghaywat@users.noreply.github.com>
2024-11-17 18:14:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "client_id" {
   description = "Azure Client ID"
+  type        = string
   default     = ""
 }
 variable "client_secret" {
   description = "Azure Client Secret"
+  type        = string
   default     = ""
 }
 variable "tenant_id" {
   description = "Azure Tenant ID"
+  type        = string
   default     = ""
 }
 variable "subscription_id" {
   description = "Azure Subscription ID"
+  type        = string
   default     = ""
 }
